b883b7ddd33684e76ca539396eb004390bc8d7a6
DataReseracher20 <s_loebenberg@stud.hwr-berlin.de>
2023-05-15 18:28:56 +0200

    changed

--- app/app.py
+++ app/app.py
@@ -10,7 +10,6 @@ st.write("Unlocking Lifetimes: Visualizing Progress in Longevity and Poverty Era
 
 
 #data
-
 # Load the CSV files into DataFrames
 df_population = pd.read_csv('population.csv')
 df_life_expectancy = pd.read_csv('life_expectancy.csv')
@@ -21,20 +20,14 @@ df_population_filled = df_population.fillna(method='ffill')
 df_life_expectancy_filled = df_life_expectancy.fillna(method='ffill')
 df_gni_per_capita_filled = df_gni_per_capita.fillna(method='ffill')
 
-
 # Select relevant columns and rename them for each DataFrame
-df_population_tidy = df_population_filled[['country', 'year', 'population']]
-df_population_tidy = df_population_tidy.rename(columns={'population': 'population'})
+df_population_tidy = df_population_filled[['country', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']]
 
-df_life_expectancy_tidy = df_life_expectancy_filled[['country', 'year', 'life_expectancy']]
-df_life_expectancy_tidy = df_life_expectancy_tidy.rename(columns={'life_expectancy': 'life_expectancy'})
 
-df_gni_per_capita_tidy = df_gni_per_capita_filled[['country', 'year', 'gni_per_capita']]
-df_gni_per_capita_tidy = df_gni_per_capita_tidy.rename(columns={'gni_per_capita': 'gni_per_capita'})
 
 # Merge the three DataFrames based on 'country' and 'year'
-merged_df = pd.merge(df_population_tidy, df_life_expectancy_tidy, on=['country', 'year'])
-merged_df = pd.merge(merged_df, df_gni_per_capita_tidy, on=['country', 'year'])
+merged_df = pd.merge(df_population_tidy, df_life_expectancy_tidy, on=['country', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'])
+merged_df = pd.merge(merged_df, df_gni_per_capita_tidy, on=['country', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'])
 
 @st.cache
 def load_data(file_path):
@@ -63,5 +56,13 @@ df_tidy = preprocess_data(df_merged)
 # Display the processed DataFrame
 
 values = df_tidy
-st.line_chart(values)
+'''st.line_chart(values)-->bubble chart The bubble chart should show the following KPIs:
+
+On the x-axis: the logarithmic Gross Natition Income (GNI) per captia (inflation-adjusted and converted to $ based on purchasing power parity (PPP)). The maximal x value should be constant independent what you select, so that you can more easily compare the charts.
+On the y-axis: Life expectancy
+Size of the bubble: population
+Color: Country
+# st.slider () to control the year
+# multiselect widget for selecting one or more countries
+'''
 
